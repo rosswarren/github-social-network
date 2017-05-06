7454d5b8e41ac431b0ed8501596c71f895333bd3
Ross Warren <rosswarren4@gmail.com>
2017-05-06 18:55:00 +0100

    react router dom

--- src/views/home-view.js
+++ src/views/home-view.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import { Link, withRouter } from 'react-router-dom';
 
 import Menu from '../menu';
 
